a2e8e9c8ba4b576fd2d723bd9a7b8a491cae6b12
Wes Higbee <wes.mcclure@gmail.com>
2023-08-27 13:15:10 -0500

    test to ensure yuv420 is the final file format

--- tests/test_pix_fmt.py
+++ tests/test_pix_fmt.py
@@ -11,6 +11,7 @@ from moviepy.video.io.VideoFileClip import VideoFileClip
 from moviepy.video.VideoClip import ColorClip
 
 from .test_helper import TMP_DIR
+from .test_ffprobe import ffprobe_stream
 
 
 def test_read_pix_fmt_defaults_to_rgb24():
@@ -37,6 +38,17 @@ def test_read_pix_fmt_yuv444p():
 
     # todo convert to rgb24 and compare?
     close_all_clips(locals())
+
+
+def test_write_pix_fmt_rgb24_to_yuv420p():
+    clip = VideoFileClip('media/big_buck_bunny_432_433.webm')
+    assert clip.reader.pix_fmt == 'rgb24'
+
+    tmp_video_output = os.path.join(TMP_DIR, "test.mp4")
+    clip.write_videofile(tmp_video_output, codec='libx264', output_pix_fmt='yuv420p')
+
+    entries = ffprobe_stream(tmp_video_output)
+    assert entries['pix_fmt'] == 'yuv420p'
 
 
 # TODO yuv422p (depth is 2) and yuv420p (depth is 1.5), requires further testing (esp around reading frames)
