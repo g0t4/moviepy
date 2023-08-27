905dc89c997b045fe972c9893f196399775ddbab
Wes Higbee <wes.mcclure@gmail.com>
2023-08-27 11:05:26 -0500

    testing of frame size

--- tests/test_pix_fmt.py
+++ tests/test_pix_fmt.py
@@ -17,7 +17,11 @@ def test_read_pix_fmt_defaults_to_rgb24():
     clip = VideoFileClip('media/big_buck_bunny_432_433.webm')
     assert clip.reader.pix_fmt == 'rgb24'
     assert clip.reader.depth == 3
-    # todo assertion on frame size?
+
+    # check frame size
+    frame0 = clip.get_frame(0)
+    assert frame0.shape == (720, 1280, 3)
+
     close_all_clips(locals())
 
 
@@ -25,10 +29,16 @@ def test_read_pix_fmt_yuv444p():
     clip = VideoFileClip('media/big_buck_bunny_432_433.webm', pix_fmt='yuv444p')
     assert clip.reader.pix_fmt == 'yuv444p'
     assert clip.reader.depth == 3
-    # todo assertions on frame size
+
+    # check frame size
+    frame0 = clip.get_frame(0)
+    assert frame0.shape == (720, 1280, 3)
+
     # todo convert to rgb24 and compare?
     close_all_clips(locals())
 
+
+# TODO yuv422p (depth is 2) and yuv420p (depth is 1.5), requires further testing (esp around reading frames)
 
 if __name__ == '__main__':
     pytest.main()
