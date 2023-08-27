36bb78e9f9ffb3b2becf6f73c2b9a13886561c9e
Wes Higbee <wes.mcclure@gmail.com>
2023-08-27 14:25:02 -0500

    work on passthrough, always increases duration which is likely from ffmpeg thinking things need converted so I will track that down later

--- tests/test_pix_fmt.py
+++ tests/test_pix_fmt.py
@@ -55,20 +55,25 @@ def test_write_pix_fmt_rgb24_to_yuv420p(yuv_pix_fmt):
 def test_read_yuv444p_and_write_to_yuv444p():
     clip = VideoFileClip('media/big_buck_bunny_432_433.webm', pix_fmt='yuv444p')
     assert clip.reader.pix_fmt == 'yuv444p'
+    assert clip.duration == 1.0
 
     tmp_video_output = os.path.join(TMP_DIR, "test.mp4")
-    clip.write_videofile(tmp_video_output, codec='libx264', raw_pix_fmt='yuv444p', output_pix_fmt='yuv444p')
+    clip.write_videofile(tmp_video_output, codec='libx264', preset=None, raw_pix_fmt='yuv444p', output_pix_fmt='yuv444p')
 
     entries = ffprobe_stream(tmp_video_output)
     assert entries['pix_fmt'] == 'yuv444p'
 
-    # todo assert same frames or CLOSE to it... why can't we preserve the original frame format? do I need to set rawvideo to libx264 instead?
-    # import numpy as np
-    # clip2 = VideoFileClip(tmp_video_output, pix_fmt='yuv420p')
-    # original_frame0 = clip.get_frame(0)
-    # new_frame0 = clip2.get_frame(0)
-    # assert original_frame0.shape == new_frame0.shape
-    # assert np.array_equal(original_frame0, new_frame0)
+    clip2 = VideoFileClip(tmp_video_output, pix_fmt='yuv444p')
+    assert clip2.duration == 1.0  # FAILs, why is length increasing when I write the file? even if I use ffmpeg at command line to go from yuv420p to yuv444p for this same original file, the result is longer? this must be "spreading" values out and causing the failure to compare actual frames?
+    return
+    original_frame0 = clip.get_frame(0)
+    new_frame0 = clip2.get_frame(0)
+    assert original_frame0.shape == new_frame0.shape
+    # assert np.allclose(original_frame0, new_frame0)
+    #
+    # from PIL import Image
+    # Image.fromarray(np.hstack([original_frame0, new_frame0])).show()
+    # Image.fromarray(np.array(original_frame0) - np.array(new_frame0)).show()
 
 
 if __name__ == '__main__':
