a782b3548811e434218013bd5bdd2c0d1cbb473e
Wes Higbee <wes.mcclure@gmail.com>
2023-08-27 10:55:29 -0500

    setup pix_fmt testing

---
+++ tests/test_pix_fmt.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""Video file clip tests meant to be run with pytest."""
+import os
+import sys
+
+import pytest
+
+from moviepy.utils import close_all_clips
+from moviepy.video.compositing.CompositeVideoClip import clips_array
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.video.VideoClip import ColorClip
+
+from .test_helper import TMP_DIR
+
+
+def test_read_pix_fmt_defaults_to_rgb24():
+    clip = VideoFileClip('media/big_buck_bunny_432_433.webm')
+    assert clip.reader.pix_fmt == 'rgb24'
+    close_all_clips(locals())
+
+
+if __name__ == '__main__':
+    pytest.main()
