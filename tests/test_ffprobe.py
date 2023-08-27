610bddde016252fe9b2eb491f8d481c8b0fcd2ad
Wes Higbee <wes.mcclure@gmail.com>
2023-08-27 13:12:31 -0500

    rewrite to grab all stream entries

--- tests/test_ffprobe.py
+++ tests/test_ffprobe.py
@@ -1,24 +1,24 @@
 import subprocess
 
 
-def test_ffprobe():
-    out = ffprobe_video('media/big_buck_bunny_432_433.webm')
-    print(out)
+def test_ffprobe_stream():
+    out = ffprobe_stream('media/big_buck_bunny_432_433.webm')
+    print(out)  # review test console output to see possibilities
     assert out['pix_fmt'] == "yuv420p"
     assert out['width'] == "1280"
     assert out['height'] == "720"
     assert out['r_frame_rate'] == "24/1"
 
 
-def ffprobe_video(video_path, stream="v:0") -> dict[str, str]:
+def ffprobe_stream(video_path, stream="v:0") -> dict[str, str]:
+    """
+      stream
+        parse stream entires for this single stream into a dict, defaults to the first video stream
+    """
+
     result = {}
     try:
-        # ** add other entries as needed
-        # list possibilities with: ffprobe -hide_banner -show_entries stream media/big_buck_bunny_432_433.webm 
-        entries = ['width', 'height', 'r_frame_rate', 'pix_fmt']
-        entries_arg = "stream=" + ','.join(entries)
-
-        cmd = ['ffprobe', '-hide_banner', '-v', 'error', '-select_streams', stream, '-show_entries', entries_arg, '-of', 'default=noprint_wrappers=1', video_path]
+        cmd = ['ffprobe', '-hide_banner', '-v', 'error', '-select_streams', stream, '-show_entries', "stream", '-of', 'default=noprint_wrappers=1', video_path]
 
         ffprobe_output = subprocess.check_output(cmd)
 
@@ -28,8 +28,8 @@ def ffprobe_video(video_path, stream="v:0") -> dict[str, str]:
         #   pix_fmt=yuv420p
         #   ...
 
-        output_lines = ffprobe_output.decode('utf-8').strip().splitlines()
-        for line in output_lines:
+        stream_entries = ffprobe_output.decode('utf-8').strip().splitlines()
+        for line in stream_entries:
             key, value = line.split('=')
             result[key] = value
     except subprocess.CalledProcessError:
