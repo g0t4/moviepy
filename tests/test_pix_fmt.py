07ac5a65c4f5cd78cb744186ec3549c05544191b
Wes Higbee <wes.mcclure@gmail.com>
2023-08-27 13:54:08 -0500

    start on a test of yuv444p pass through and back out to yuv444p ... just need to find a way to get frames not to differ so I can compare them too and ensure same frame input is the same one output... assuming that is possible... need to figure out how to pass raw h264 vcodec out to ffmpeg

--- tests/test_pix_fmt.py
+++ tests/test_pix_fmt.py
@@ -2,7 +2,6 @@
 """Video file clip tests meant to be run with pytest."""
 import os
 import sys
-
 import pytest
 
 from moviepy.utils import close_all_clips
@@ -30,6 +29,7 @@ def test_read_pix_fmt_yuv444p():
     clip = VideoFileClip('media/big_buck_bunny_432_433.webm', pix_fmt='yuv444p')
     assert clip.reader.pix_fmt == 'yuv444p'
     assert clip.reader.depth == 3
+    # TODO yuv422p (depth is 2) and yuv420p (depth is 1.5), requires further testing (esp around reading frames)
 
     # check frame size
     frame0 = clip.get_frame(0)
@@ -52,7 +52,24 @@ def test_write_pix_fmt_rgb24_to_yuv420p(yuv_pix_fmt):
     assert entries['pix_fmt'] == yuv_pix_fmt
 
 
-# TODO yuv422p (depth is 2) and yuv420p (depth is 1.5), requires further testing (esp around reading frames)
+def test_read_yuv444p_and_write_to_yuv444p():
+    clip = VideoFileClip('media/big_buck_bunny_432_433.webm', pix_fmt='yuv444p')
+    assert clip.reader.pix_fmt == 'yuv444p'
+
+    tmp_video_output = os.path.join(TMP_DIR, "test.mp4")
+    clip.write_videofile(tmp_video_output, codec='libx264', raw_pix_fmt='yuv444p', output_pix_fmt='yuv444p')
+
+    entries = ffprobe_stream(tmp_video_output)
+    assert entries['pix_fmt'] == 'yuv444p'
+
+    # todo assert same frames or CLOSE to it... why can't we preserve the original frame format? do I need to set rawvideo to libx264 instead?
+    # import numpy as np
+    # clip2 = VideoFileClip(tmp_video_output, pix_fmt='yuv420p')
+    # original_frame0 = clip.get_frame(0)
+    # new_frame0 = clip2.get_frame(0)
+    # assert original_frame0.shape == new_frame0.shape
+    # assert np.array_equal(original_frame0, new_frame0)
+
 
 if __name__ == '__main__':
     pytest.main()
